b80d9f363e2bd1b09aa61184eba4b322eeaf0a7c
Олег Слюсар <eternityonly1@gmail.com>
2023-10-23 13:24:47 +0300

    Виконання завдань

---
+++ task-2.js
@@ -0,0 +1,23 @@
+function formatMessage (message, maxLength)
+{
+
+  const endIndex = maxLength;
+  const slicedMessage = message.slice(0, endIndex);
+
+  if (message.length <= maxLength) {
+    return message;
+  }
+  else {
+    return `${slicedMessage}...`;
+  }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+
+
+
---
+++ task-3.js
@@ -0,0 +1,14 @@
+function checkForSpam(message) {
+
+  const messageInput = message.toLowerCase();
+  return (messageInput.includes(`spam`) || messageInput.includes(`sale`)) ? `true` : `false`;
+
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
